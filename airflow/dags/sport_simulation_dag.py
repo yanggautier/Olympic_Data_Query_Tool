8a973c54cd315e18344b2d0a73cef84b51433847
yanggautier <yangguole@outlook.com>
2025-06-09 00:15:07 +0200

    Airflow fonctionnel

---
+++ airflow/dags/sport_simulation_dag.py
@@ -0,0 +1,35 @@
+from pendulum import datetime
+from datetime import timedelta
+import os
+from airflow.sdk import dag, task
+from utils.simulation_data import generate_sports
+from sqlalchemy import create_engine
+
+
+@dag(
+    start_date=datetime(2025, 6, 8),
+    schedule=timedelta(minutes=2),
+    end_date=datetime(2026, 6, 9),
+    catchup=False,
+    tags=["sport", "olympic", "data"]
+)
+def olympic_data_generation():
+    @task()
+    def get_generate_data():
+        df = generate_sports(context={"logical_date": 1988})
+        return df
+
+    @task()
+    def load(df):
+        connect_string = os.getenv("DB_CONNECT_STRING")
+        if connect_string:
+            engine = create_engine(connect_string)
+            df.to_sql('fact_resultats_epreuves', engine, if_exists='append', index=False)
+        else:
+            raise Exception("Impossibe de connecter à la base de données")
+
+    data = get_generate_data()
+    load(data)
+
+
+olympic_data_generation()
